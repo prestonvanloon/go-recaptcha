5342a1ccaea333868b08030821572ab787cda3a0
Preston Van Loon <preston@prysmaticlabs.com>
2019-02-17 13:02:51 -0500

    No global, no confirm method, make check public

--- recaptcha.go
+++ recaptcha.go
@@ -3,7 +3,6 @@
 // This package is designed to be called from within an HTTP server or web framework
 // which offers reCaptcha form inputs and requires them to be evaluated for correctness
 //
-// Edit the recaptchaPrivateKey constant before building and using
 package recaptcha
 
 import (
@@ -26,46 +25,28 @@ type RecaptchaResponse struct {
 
 const recaptchaServerName = "https://www.google.com/recaptcha/api/siteverify"
 
-var recaptchaPrivateKey string
+type Recaptcha struct {
+  RecaptchaPrivateKey string
+}
 
-// check uses the client ip address, the challenge code from the reCaptcha form,
+// Check uses the client ip address, the challenge code from the reCaptcha form,
 // and the client's response input to that challenge to determine whether or not
 // the client answered the reCaptcha input question correctly.
-// It returns a boolean value indicating whether or not the client answered correctly.
-func check(remoteip, response string) (r RecaptchaResponse, err error) {
+func (r *Recaptcha) Check(remoteip, response string) (*RecaptchaResponse, error) {
 	resp, err := http.PostForm(recaptchaServerName,
-		url.Values{"secret": {recaptchaPrivateKey}, "remoteip": {remoteip}, "response": {response}})
+	   url.Values{"secret": {recaptchaPrivateKey:r.RecaptchaPrivateKey}, "remoteip": {remoteip}, "response": {response}})
 	if err != nil {
 		log.Printf("Post error: %s\n", err)
-		return
+		return nil, err
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Println("Read error: could not read body: %s", err)
-		return
+		return nil, err
 	}
 	err = json.Unmarshal(body, &r)
 	if err != nil {
-		log.Println("Read error: got invalid JSON: %s", err)
-		return
+		return nil, err
 	}
-	return
-}
-
-// Confirm is the public interface function.
-// It calls check, which the client ip address, the challenge code from the reCaptcha form,
-// and the client's response input to that challenge to determine whether or not
-// the client answered the reCaptcha input question correctly.
-// It returns a boolean value indicating whether or not the client answered correctly.
-func Confirm(remoteip, response string) (result bool, err error) {
-	resp, err := check(remoteip, response)
-	result = resp.Success
-	return
-}
-
-// Init allows the webserver or code evaluating the reCaptcha form input to set the
-// reCaptcha private key (string) value, which will be different for every domain.
-func Init(key string) {
-	recaptchaPrivateKey = key
+	return body, err
 }
